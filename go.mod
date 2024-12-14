33dd37a218bf5fa792a3b25323ac3d1dfb21e3f7
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-14 14:29:36 +0545

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module geo
 
 go 1.23
+
+require (
+	github.com/go-gota/gota v0.12.0
+	github.com/stretchr/testify v1.10.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	gonum.org/v1/gonum v0.9.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ pkg/country/country_test.go
@@ -0,0 +1,66 @@
+package country
+
+import (
+	"fmt"
+	"github.com/go-gota/gota/dataframe"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestSanity(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(123, 123, "they should be equal")
+}
+
+func TestCsvCountry(t *testing.T) {
+	csvStr := `
+Country,Date,Age,Amount,Id
+"United States",2012-02-01,50,112.1,01234
+"United States",2012-02-01,32,321.31,54320
+"United Kingdom",2012-02-01,17,18.2,12345
+"United States",2012-02-01,32,321.31,54320
+"United Kingdom",2012-02-01,NA,18.2,12345
+"United States",2012-02-01,32,321.31,54320
+"United States",2012-02-01,32,321.31,54320
+Spain,2012-02-01,66,555.42,00241
+`
+	df := dataframe.ReadCSV(strings.NewReader(csvStr))
+	fmt.Println(df)
+}
+func TestCountryJson(t *testing.T) {
+	csvStr := `
+Country,Date,Age,Amount,Id
+"United States",2012-02-01,50,112.1,01234
+"United States",2012-02-01,32,321.31,54320
+"United Kingdom",2012-02-01,17,18.2,12345
+"United States",2012-02-01,32,321.31,54320
+"United Kingdom",2012-02-01,NA,18.2,12345
+"United States",2012-02-01,32,321.31,54320
+"United States",2012-02-01,32,321.31,54320
+Spain,2012-02-01,66,555.42,00241
+`
+	df := dataframe.ReadJSON(strings.NewReader(csvStr))
+	fmt.Println(df)
+}
+
+//func TestAdd(t *testing.T) {
+//	assert.Equal(t, 123, 123, "they should be equal")
+//	//testCases := []struct {
+//	//	a        int
+//	//	b        int
+//	//	expected int
+//	//}{
+//	//	{2, 3, 5},
+//	//	{-1, -1, -2},
+//	//	{0, 0, 0},
+//	//	{100, 200, 300},
+//	//}
+//	//
+//	//for _, tc := range testCases {
+//	//	result := Add(tc.a, tc.b)
+//	//	if result != tc.expected {
+//	//		t.Errorf("Add(%d, %d) = %d; want %d", tc.a, tc.b, result, tc.expected)
+//	//	}
+//	//}
+//}
