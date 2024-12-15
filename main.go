3b5d247308656743f97be445d76c36cce88dfd0a
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-15 15:39:15 +0545

    chore: add clipboard dependency and refactor main.go

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
-	"geo/pkg/country"
 	"geo/pkg/tui"
 )
 
@@ -11,27 +9,28 @@ import (
 var version string
 
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s! version [%s]\n", s, version)
-	countryCode := "41"
-	fil := country.FilterCountryByCountryCodeDataFrame(countryCode)
-	sel := fil.Drop([]string{"EquivalentFipsCode", "Postal Code Regex", "Postal Code Format"}).Drop("Area(in sq km)")
-
-	//sel := fil.Select([]string{"ISO", "ISO3", "ISO-Numeric", "fips",
-	//	"Country",
-	//	"Capital",
-	//	//,
-	//	"Population",
-	//	//"Continent	tld	CurrencyCode",
-	//	"CurrencyName",
-	//	"Phone",
-	//	//"Postal Code Format	Postal Code Regex",
-	//	"Languages",
-	//	"geonameid",
-	//	"neighbours",
-	//	//"EquivalentFipsCode",
-	//})
-	result := tui.PrintDataframe(sel)
-	fmt.Println(result)
+	tui.FilterPhone()
+	//s := "gopher"
+	//fmt.Printf("Hello and welcome, %s! version [%s]\n", s, version)
+	//countryCode := "41"
+	//fil := country.FilterCountryByCountryCodeDataFrame(countryCode)
+	//sel := fil.Drop([]string{"EquivalentFipsCode", "Postal Code Regex", "Postal Code Format"}).Drop("Area(in sq km)")
+	//
+	////sel := fil.Select([]string{"ISO", "ISO3", "ISO-Numeric", "fips",
+	////	"Country",
+	////	"Capital",
+	////	//,
+	////	"Population",
+	////	//"Continent	tld	CurrencyCode",
+	////	"CurrencyName",
+	////	"Phone",
+	////	//"Postal Code Format	Postal Code Regex",
+	////	"Languages",
+	////	"geonameid",
+	////	"neighbours",
+	////	//"EquivalentFipsCode",
+	////})
+	//result := tui.PrintDataframe(sel)
+	//fmt.Println(result)
 
 }
