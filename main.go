e28985fcb442b115ab693e33556556dd2a831d20
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-15 10:34:48 +0545

    feat(table): add table display feature with data rows

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"geo/pkg/table"
 )
 
 //go:embed VERSION
 var version string
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
 	s := "gopher"
 	fmt.Printf("Hello and welcome, %s! version [%s]\n", s, version)
-
+	table.TBBB()
 }
