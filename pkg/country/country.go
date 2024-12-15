5db1f641562173b90efa99aa3a94033057b777d7
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-15 16:20:07 +0545

    feat(country): add substring filter for country codes

--- pkg/country/country.go
+++ pkg/country/country.go
@@ -131,8 +131,19 @@ func LoadCountryDataFrame() dataframe.DataFrame {
 }
 func FilterCountryByCountryCodeDataFrame(countryCode string) dataframe.DataFrame {
 	df := ReadCountryAsDataFrame()
+	stringContains := func(substring string) func(el series.Element) bool {
+		return func(el series.Element) bool {
+			if el.Type() == series.String {
+				if val, ok := el.Val().(string); ok {
+					return strings.Contains(val, substring)
+				}
+			}
+			return false
+		}
+	}
+	fil := df.Filter(dataframe.F{Colname: "Phone", Comparator: series.CompFunc, Comparando: stringContains(countryCode)})
 
-	fil := df.Filter(dataframe.F{Colname: "Phone", Comparator: series.Eq, Comparando: countryCode})
+	//fil := df.Filter(dataframe.F{Colname: "Phone", Comparator: series.Eq, Comparando: countryCode})
 	return fil
 }
 
