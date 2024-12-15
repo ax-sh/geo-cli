365427fe3f53f8bc16daa1b45ac96415aee5f36a
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-15 15:26:56 +0545

    feat(tui): add row styling to PrintDataframe function

--- pkg/tui/table.go
+++ pkg/tui/table.go
@@ -6,11 +6,27 @@ import (
 	"github.com/go-gota/gota/dataframe"
 )
 
+var (
+	HeaderStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("99"))
+	EvenRowStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("242"))
+	OddRowStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("242"))
+)
+
 // PrintDataframe printable pretty tui
 func PrintDataframe(df dataframe.DataFrame) *table.Table {
 	prettyTable := table.New().
 		Border(lipgloss.NormalBorder()).
 		BorderStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("99"))).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			switch {
+			case row == 0:
+				return HeaderStyle
+			case row%2 == 0:
+				return EvenRowStyle
+			default:
+				return OddRowStyle
+			}
+		}).
 		Headers(df.Names()...).
 		Rows(df.Records()[1:]...)
 	return prettyTable
