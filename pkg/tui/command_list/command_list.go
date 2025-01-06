571475db786d46cd7740feb73b8270b93356327e
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-06 16:33:19 +0545

    feat(command_list): add choice handling in model logic

--- pkg/tui/command_list/command_list.go
+++ pkg/tui/command_list/command_list.go
@@ -2,6 +2,7 @@ package command_list
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/charmbracelet/bubbles/list"
@@ -20,7 +21,9 @@ func (i item) Description() string { return i.desc }
 func (i item) FilterValue() string { return i.title }
 
 type model struct {
-	list list.Model
+	list     list.Model
+	choice   string
+	quitting bool
 }
 
 func (m model) Init() tea.Cmd {
@@ -31,6 +34,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		if msg.String() == "ctrl+c" {
+			m.quitting = true
+			return m, tea.Quit
+		}
+		if msg.String() == "enter" {
+			// Store the selected item's title when Enter is pressed
+			if i, ok := m.list.SelectedItem().(item); ok {
+				m.choice = i.title
+			}
 			return m, tea.Quit
 		}
 	case tea.WindowSizeMsg:
@@ -49,38 +60,33 @@ func (m model) View() string {
 
 func FooMain() {
 	items := []list.Item{
-		item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
-		item{title: "Nutella", desc: "It's good on toast"},
-		item{title: "Bitter melon", desc: "It cools you down"},
-		item{title: "Nice socks", desc: "And by that I mean socks without holes"},
-		item{title: "Eight hours of sleep", desc: "I had this once"},
-		item{title: "Cats", desc: "Usually"},
-		item{title: "Plantasia, the album", desc: "My plants love it too"},
-		item{title: "Pour over coffee", desc: "It takes forever to make though"},
-		item{title: "VR", desc: "Virtual reality...what is there to say?"},
-		item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
-		item{title: "Linux", desc: "Pretty much the best OS"},
-		item{title: "Business school", desc: "Just kidding"},
-		item{title: "Pottery", desc: "Wet clay is a great feeling"},
-		item{title: "Shampoo", desc: "Nothing like clean hair"},
-		item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
-		item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
-		item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
-		item{title: "Stickers", desc: "The thicker the vinyl the better"},
-		item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
-		item{title: "Warm light", desc: "Like around 2700 Kelvin"},
-		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
-		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
-		item{title: "Terrycloth", desc: "In other words, towel fabric"},
+		item{title: "Phone", desc: "Find country by country code"},
+		item{title: "tld", desc: "Search by Top layer domain"},
 	}
 
-	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
-	m.list.Title = "My Fave Things"
+	m := model{
+		list: list.New(items, list.NewDefaultDelegate(), 0, 0),
+	}
+	m.list.Title = "Tools"
 
 	p := tea.NewProgram(m, tea.WithAltScreen())
 
-	if _, err := p.Run(); err != nil {
+	finalModel, err := p.Run()
+	if err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
+	}
+
+	// Get the selected item from the final model
+
+	if finalModel, ok := finalModel.(model); ok {
+
+		switch finalModel.choice {
+		case "tld":
+			println("foodo")
+		default:
+			log.Println("choice", finalModel.choice)
+		}
+
 	}
 }
